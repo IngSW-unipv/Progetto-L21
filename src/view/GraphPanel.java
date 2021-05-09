266128d51a4e65f83a58e6b068f68b540f300f01
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2021-05-10 00:18:42 +0200

    added live cursor tracking, for info on coordinate that is being pointed

--- src/view/GraphPanel.java
+++ src/view/GraphPanel.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.IllegalComponentStateException;
+import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -17,37 +19,46 @@ import model.functions.FunctionIF;
 @SuppressWarnings("serial")
 public class GraphPanel extends JPanel implements Observer, KeyListener{
 
+	
 	private static final int WIDTH = 600;
 	private static final int HEIGHT = 600;
 	
-	//(for now) hardcoded parameters for the graph 
-		int xMin = -20;
-		int xMax = 20;
-		int yMin = -18;
-		int yMax = 18;
-		double step = 0.1;
-		Color BG_COLOR =  Color.black;
-		Color AXES_COLOR = Color.yellow;
-		Color FUNCTIONS_COLOR = Color.red;
-		BasicStroke AXES_STROKE = new BasicStroke(1);
-		BasicStroke FUNCTIONS_STROKE = new BasicStroke(2);
-		
+	
+    //(for now) hardcoded parameters for the graph 
+	int xMin = -20;
+	int xMax = 20;
+	int yMin = -18;
+	int yMax = 18;
+	double step = 0.1;
+	Color BG_COLOR =  Color.black;
+	Color AXES_COLOR = Color.yellow;
+	Color FUNCTIONS_COLOR = Color.red;
+	BasicStroke AXES_STROKE = new BasicStroke(1);
+	BasicStroke FUNCTIONS_STROKE = new BasicStroke(2);
 
-		
-		//this list is to store functions to be plotted cumulatively
-		//on the same instance of the graph.
-		ArrayList<FunctionIF> functionsOnDisplay = new ArrayList<FunctionIF>();
-		
+	//this list is to store functions to be plotted cumulatively
+	//on the same instance of the graph.
+	ArrayList<FunctionIF> functionsOnDisplay = new ArrayList<FunctionIF>();
+	
+	//this is a reference to this panel (to be used in separate thread)
+	GraphPanel autoreferenceToGraphPanel;
+	
+	//this is the (Cartesian) coordinate pointed to by the cursor on screen
+	//it's constantly kept up to date by the HoveringCoordinatesThread
+	volatile Coordinate cursorCartesianCoord;
+					
+			
 
 	public GraphPanel() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		
+		//get a reference to this panel (to be used in separate thread)
+		autoreferenceToGraphPanel = this;
+		
+		//start the thread that determines the cursorCartesianCoord
+		new HoveringCoordinatesThread().start();	
 	}
 
-	
-	
-	
-	
-	
 	
 
 	/**
@@ -62,16 +73,34 @@ public class GraphPanel extends JPanel implements Observer, KeyListener{
 		int pixelX = (int)Math.round(( x  - xMin)*unit);
 		int pixelY = (int)Math.round((yMax - y)*unit);
 
-		
 		return new Point(pixelX, pixelY);
 	}
 
+	
+	/**
+	 * Converts a pixel point to a Cartesian point.
+	 *  
+	 * This is used to determine a Cartesian coordinate, 
+	 * from the pixel-point (mouse-position) the cursor is hovering on.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	
+	public Coordinate pixelToCartesian(int x, int y) {
+		double unit = getWidth()/(xMax - xMin); //pixels on the interval
+		return new Coordinate( x/unit +xMin  , yMax - y/unit);
+	}
+	
+	
 	
 	@Override
 	public void paint(Graphics arg0) {
 		
 		//cast the Graphics obj to a Graphics2D obj
 		Graphics2D g2d = (Graphics2D)arg0;
+		
 		
 		//paint the background
 		g2d.setColor(BG_COLOR);
@@ -84,8 +113,14 @@ public class GraphPanel extends JPanel implements Observer, KeyListener{
 		for(FunctionIF functionOnDisplay : functionsOnDisplay) {
 			plotFunction(functionOnDisplay, g2d);
 		}
-
+		
+		//paints the coordinate that the cursor is hovering on
+		Point p = cartesianToPixel(cursorCartesianCoord.x, cursorCartesianCoord.y);
+		g2d.drawString("("+cursorCartesianCoord.x+", "+cursorCartesianCoord.y+")", p.x, p.y);
+		
 	}
+	
+	
 	
 	
 	
@@ -288,6 +323,74 @@ public class GraphPanel extends JPanel implements Observer, KeyListener{
 	public void keyTyped(KeyEvent arg0) {
 		
 	}
+	
+	
+	
+	
+	/**
+	 * Get the panel's location on the screen. 
+	 * NB: this is the coordinate of the TOP-LEFT corner of the PANEL.
+	 */
+	public Point getPanelLocation() {
+		Point panelLocation = new Point(0,0);
+		try {
+			panelLocation = autoreferenceToGraphPanel.getLocationOnScreen();
+		}catch(IllegalComponentStateException e) {
+			
+		}	
+		return panelLocation;
+	}
+	
+	
+	
+	
+	
+	/**
+	 * This thread updates the coordinate that the cursor is hovering
+	 * on at any given moment, and tells the graph to paint it right
+	 * next to the cursor. 
+	 * 
+	 * @author L21 Team
+	 *
+	 */
+	class HoveringCoordinatesThread extends Thread{
+
+		@Override
+		public void run() {
+			
+			while(true) {
+				
+				//get the cursor's position RELATIVE TO THE WHOLE SCREEN
+				Point mouseCoord = MouseInfo.getPointerInfo().getLocation();
+				
+				//get the location of the panel
+				Point panelLocation = getPanelLocation();
+
+				//get the position of the cursor RELATIVE TO THE PANEL
+				int mouseOnPanelX = mouseCoord.x - panelLocation.x;
+				int mouseOnPanelY = mouseCoord.y - panelLocation.y;
+				//covert it to the corresponding Cartesian coordinate
+				cursorCartesianCoord = pixelToCartesian(mouseOnPanelX, mouseOnPanelY);
+
+				
+				//redraw the panel (updating the coordinate displayed)
+				SwingUtilities.invokeLater(new Runnable() {
+					@Override
+					public void run() {
+						autoreferenceToGraphPanel.repaint();
+					}
+				});
+				
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
 
 
 	
