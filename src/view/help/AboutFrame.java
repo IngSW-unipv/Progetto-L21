f6f113744c4c84a5c6fbc5d658df73892ed2c2a4
aiman <luxlunarislabs@gmail.com>
2021-06-22 15:15:50 +0200

    more refactoring of popup frames (help and about)

--- src/view/help/AboutFrame.java
+++ src/view/help/AboutFrame.java
@@ -1,20 +1,14 @@
 package view.help;
 
-import java.awt.Dimension;
-import java.awt.GraphicsConfiguration;
-import java.awt.HeadlessException;
-import java.awt.Toolkit;
 import java.io.IOException;
 
-import javax.swing.Box;
-import javax.swing.JEditorPane;
-import javax.swing.JFrame;
+
 import javax.swing.JScrollPane;
 
 public class AboutFrame extends AbstractPopupFrame {
 
 	public AboutFrame() {
-		super("Help", "./images/help.JPG");
+		super(LANGUAGE_MODULE.get("help_menu_about"), "./images/help.JPG");
 
 		try {
 			display.setPage(LANGUAGE_MODULE.get("help_doc"));
--- src/view/help/WelcomeFrame.java
+++ src/view/help/WelcomeFrame.java
@@ -1,21 +1,15 @@
 package view.help;
 
-import java.awt.Dimension;
-import java.awt.GraphicsConfiguration;
-import java.awt.HeadlessException;
-import java.awt.Label;
-import java.awt.Toolkit;
+
 import java.io.IOException;
 
-import javax.swing.Box;
-import javax.swing.JEditorPane;
-import javax.swing.JFrame;
+
 import javax.swing.JScrollPane;
 
 public class WelcomeFrame extends AbstractPopupFrame {
 
 	public WelcomeFrame() {
-	super("Welcome", "./images/welcome.JPG");
+	super(LANGUAGE_MODULE.get("help_menu_welcome"), "./images/welcome.JPG");
 
 	
 	try {
