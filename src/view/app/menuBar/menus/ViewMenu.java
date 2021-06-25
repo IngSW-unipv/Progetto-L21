8c70af5e3b2bc2d9801ef22984ac24e7cdaed079
aiman <luxlunarislabs@gmail.com>
2021-06-26 00:34:11 +0200

    fixed max num modifying bug

--- src/view/app/menuBar/menus/ViewMenu.java
+++ src/view/app/menuBar/menus/ViewMenu.java
@@ -147,7 +147,7 @@ public class ViewMenu extends AbstractMenu {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				String functionsNumber = JOptionPane.showInputDialog(LANGUAGE_MODULE.get("view_menu_max_insert_new_max_functions"));
-				if(functionsNumber==null) {
+				if(!functionsNumber.matches("\\d+")) {
 					return;
 				}
 				ModuleManager.getInstance().getModule("graph").put("MAX_INSERTABLE_FUNCTIONS", functionsNumber);	
