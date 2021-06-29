8135d2e177cf4b49e4996706e671bb3a8a2e66a5
aiman <luxlunarislabs@gmail.com>
2021-06-29 20:55:20 +0200

    removed debug printout

--- src/view/app/GraphController.java
+++ src/view/app/GraphController.java
@@ -111,7 +111,6 @@ public class GraphController {
 
 	// TODO !!
 	public void toggleHoverCoordinates() {
-		System.out.println(graphPanel.HOVER_COORDINATES);
 		graphModule.put("HOVER_COORDINATES", (!graphPanel.HOVER_COORDINATES)+"");
 		graphPanel.repaint();
 	}
